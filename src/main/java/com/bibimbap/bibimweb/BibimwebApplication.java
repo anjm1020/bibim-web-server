fd4aceed6c3fbcdfa0a464f41c6b45aac8be83d7
jaemin <anjm1020@gmail.com>
2022-07-11 16:02:15 +0900

    Init : init project

---
+++ src/main/java/com/bibimbap/bibimweb/BibimwebApplication.java
@@ -0,0 +1,13 @@
+package com.bibimbap.bibimweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibimwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibimwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibimbap/bibimweb/BibimwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibimbap.bibimweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibimwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
