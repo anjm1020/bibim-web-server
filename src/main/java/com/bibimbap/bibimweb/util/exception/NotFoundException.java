7ff1a1b61c67cbb6d577d478be8b128d3e17a0af
jaemin <anjm1020@gmail.com>
2022-07-29 16:14:04 +0900

    Feat : 404 예외 추가

--- src/main/java/com/bibimbap/bibimweb/util/exception/NotFoundException.java
+++ src/main/java/com/bibimbap/bibimweb/util/exception/NotFoundException.java
@@ -3,6 +3,7 @@ package com.bibimbap.bibimweb.util.exception;
 public class NotFoundException extends RuntimeException {
     public static final NotFoundException ROLE = new NotFoundException("존재하지 않는 역할입니다");
     public static final NotFoundException MEMBER = new NotFoundException("존재하지 않는 회원입니다");
+    public static final NotFoundException PROJECT_GROUP = new NotFoundException("존재하지 않는 프로젝트입니다");
     private NotFoundException(String message) {
         super(message);
     }
