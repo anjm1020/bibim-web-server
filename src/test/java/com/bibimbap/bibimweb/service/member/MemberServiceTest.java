7a0c82adbb8cac0f77e22a19eb6d8c57f6ac1639
jaemin <anjm1020@gmail.com>
2022-08-10 21:35:47 +0900

    Test : 회원 테스트 코드 리스트업

---
+++ src/test/java/com/bibimbap/bibimweb/service/member/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package com.bibimbap.bibimweb.service.member;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    @DisplayName("일반회원 생성 테스트")
+    void createMember() {
+
+    }
+
+    @Test
+    @DisplayName("운영진 생성 테스트")
+    void createAdmin() {
+
+    }
+
+    @Test
+    @DisplayName("회원 일반 필드 수정 테스트")
+    void updateNormField() {
+
+    }
+
+    @Test
+    @DisplayName("회원 리스트 - 페이지네이션 테스트")
+    void getListByPage() {
+
+    }
+
+    @Test
+    @DisplayName("운영진 리스트 - 년도별, 전체 테스트")
+    void getAdminList() {
+
+    }
+
+    @Test
+    @DisplayName("회원 삭제 시 Role 제대로 업데이트 되는지 테스트")
+    void checkRoleTableAfterDelete() {
+
+    }
+
+}
